75ded98d46060c0212f56319eb306a37460db1dd
Krowe Moh <mohammad4sure@gmail.com>
2022-11-17 18:03:26 +1100

    Update index.js
    trash code but it works :)

--- index.js
+++ index.js
@@ -11,19 +11,29 @@ function makeid(length) {
 	}
 	return result;
 }
-for (var i = 0; i < sacnum; i++) {
-	CheckSac(makeid(saclength))
+if (fs.existsSync('unused.txt') && fs.existsSync('used.txt')) {
+	for (var i = 0; i < amount; i++) { CheckSac(makeid(saclength)) }
+} else {
+	fs.appendFile('unused.txt', '', function(err) {
+		if (err) console.log(err);
+	});
+	fs.appendFile('used.txt', '', function(err) {
+		if (err) console.log(err);
+	})
+	for (var i = 0; i < amount; i++) {
+		CheckSac(makeid(saclength))
+	}
 }
 function CheckSac(sac) {
-	if (fs.readFileSync('./unused.txt').toString().split('\n').includes(sac) || fs.readFileSync('./used.txt').toString().split('\n').includes(sac))
-		axios.get(`https://fortnite-api.com/v2/creatorcode?name=${sac}`).catch(() => {
-			fs.appendFile('valid.txt', sac + '\n', function(err) {
-				if (err) console.log(err);
-			});
-		}).then(function(response) {
-			if (!response?.data) return
-			fs.appendFile('invalid.txt', response.data.data.code + '\n', function(err) {
-				if (err) console.log(err);
-			})
+	if (fs.readFileSync('./unused.txt').toString().split('\n').includes(sac) || fs.readFileSync('./used.txt').toString().split('\n').includes(sac)) return;
+	axios.get(`https://fortnite-api.com/v2/creatorcode?name=${sac}`).catch(() => {
+		fs.appendFile('unused.txt', sac + '\n', function(err) {
+			if (err) console.log(err);
+		});
+	}).then(function(response) {
+		if (!response?.data) return
+		fs.appendFile('used.txt', response.data.data.code + '\n', function(err) {
+			if (err) console.log(err);
 		})
+	})
 }
