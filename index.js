ee3bb6904bd837d62b879e43a9868270724d0e4d
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2023-07-31 22:28:12 +1000

    hm

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
 const axios = require('axios')
-const fs = require('fs');
+const fs = require('fs').promises;
 let amount = 1000
 let saclength = 3
+
 function makeid(length) {
-	var result = '';
-	var characters = 'abcdefghijklmnopqrstuvwxyz';
-	var charactersLength = characters.length;
-	for (var i = 0; i < length; i++) {
-		result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	}
-	return result;
+    var result = '';
+    var characters = 'abcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
-if (fs.existsSync('unused.txt') && fs.existsSync('used.txt')) {
-	for (var i = 0; i < amount; i++) { CheckSac(makeid(saclength)) }
-} else {
-	fs.appendFile('unused.txt', '', function(err) {
-		if (err) console.log(err);
-	});
-	fs.appendFile('used.txt', '', function(err) {
-		if (err) console.log(err);
-	})
-	for (var i = 0; i < amount; i++) {
-		CheckSac(makeid(saclength))
-	}
+
+async function CheckSac(sac, unused, used) {
+    if (unused.includes(sac) || used.includes(sac)) return;
+    try {
+        const response = await axios.get(`https://fortnite-api.com/v2/creatorcode?name=${sac}`)
+        if (response.data) {
+            await fs.appendFile('used.txt', response.data.data.code + '\n');
+            used.push(sac);
+        }
+    } catch (error) {
+        await fs.appendFile('unused.txt', sac + '\n');
+        unused.push(sac);
+    }
 }
-function CheckSac(sac) {
-	if (fs.readFileSync('./unused.txt').toString().split('\n').includes(sac) || fs.readFileSync('./used.txt').toString().split('\n').includes(sac)) return;
-	axios.get(`https://fortnite-api.com/v2/creatorcode?name=${sac}`).catch(() => {
-		fs.appendFile('unused.txt', sac + '\n', function(err) {
-			if (err) console.log(err);
-		});
-	}).then(function(response) {
-		if (!response?.data) return
-		fs.appendFile('used.txt', response.data.data.code + '\n', function(err) {
-			if (err) console.log(err);
-		})
-	})
+
+async function main() {
+    let unused = [], used = [];
+    try {
+        unused = (await fs.readFile('unused.txt')).toString().split('\n');
+        used = (await fs.readFile('used.txt')).toString().split('\n');
+    } catch (error) {
+        await fs.writeFile('unused.txt', '');
+        await fs.writeFile('used.txt', '');
+    }
+    for (var i = 0; i < amount; i++) {
+        await CheckSac(makeid(saclength), unused, used)
+    }
 }
+
+main().catch(console.error);
